01af0fd863155686797442feb397b21b8e851a9e
ontheway1215 <179023084@qq.com>
2018-06-14 10:07:37 +0800

    change input component style

--- src/components/input/src/input.vue
+++ src/components/input/src/input.vue
@@ -3,19 +3,17 @@
     class="at-input"
     :class="[
       size ? `at-input--${size}` : '',
-      status ? `at-input-${status}` : '',
+      status ? `at-input--${status}` : '',
       {
         'at-input-group': $slots.prepend || $slots.append,
         'at-input--disabled': disabled,
         'at-input--prepend': $slots.prepend,
-        'at-input--prepend': $slots.append,
+        'at-input--append': $slots.append,
         'at-input--icon': icon
       }
     ]">
     <!-- S prepend element -->
-    <div class="at-input-group__prepend" :class="{ '
-    at-input-group--button': prependButton }" v-if="$slots.
-    prepend">
+    <div class="at-input-group__prepend" :class="{ 'at-input-group--button': prependButton }" v-if="$slots.prepend">
         <slot name="prepend"></slot>
     </div>
     <!-- E prepend element -->
@@ -40,13 +38,11 @@
     <!-- E input -->
 
     <!-- S icon -->
-    <i class="at-input__icon icon" :class="iconClass"
-    v-if="icon"></i>
+    <i class="at-input__icon icon" :class="iconClass" v-if="icon"></i>
     <!-- E icon -->
 
     <!-- S append element -->
-    <div class="at-input-group_append" :class="{ '
-    at-input-group--button': appendButton }" v-if="$slots.append">
+    <div class="at-input-group__append" :class="{ 'at-input-group--button': appendButton }" v-if="$slots.append">
       <slot name="append"></slot>
     </div>
     <!-- E append element -->
@@ -54,82 +50,79 @@
 </template>
 
 <script>
+import Emitter from 'c-ui/src/mixins/emitter'
 export default {
-  import Emitter from 'c-ui/src/mixins/emitter'
-
-  export default {
-    name: 'AtInput',
-    mixins: [Emitter],
-    props: {
-      type: {
-        type: String,
-        default: 'text'
-      },
-      value: [String, Number],
-      name: String,
-      placeholder: String,
-      size: String,
-      status: String,
-      icon: String,
-      prependButton: {
-        type: Boolean,
-        default: false
-      },
-      appendButton: {
-        type: Boolean,
-        default: false
-      },
-      readonly: {
-        type: Boolean,
-        default: false
-      },
-      disabled: {
-        type: Boolean,
-        default: false
-      },
-      autofocus: {
-        type: Boolean,
-        default: false
-      },
-      maxlength: Number,
-      minlength: Number,
-      max: Number,
-      min: Number
+  name: 'AtInput',
+  mixins: [Emitter],
+  props: {
+    type: {
+      type: String,
+      default: 'text'
     },
-    computed: {
-      iconClass () {
-        const name = this.icon || this.status
-        return name ? `icon-${name}` : ''
-      }
+    value: [String, Number],
+    name: String,
+    placeholder: String,
+    size: String,
+    status: String,
+    icon: String,
+    prependButton: {
+      type: Boolean,
+      default: false
     },
-    data () {
-      return {
-        currentValue: this.value
-      }
+    appendButton: {
+      type: Boolean,
+      default: false
     },
-    watch: {
-      value (val) {
-        this.setCurrentValue(val)
-      }
+    readonly: {
+      type: Boolean,
+      default: false
     },
-    methods: {
-      handleFocus (evt) {
-        this.$emit('focus', evt)
-      },
-      handleBlur (evt) {
-        this.$emit('blur', evt)
-        this.dispatch('AtFormItem', 'on-form-item-blur', this.currentValue)
-      },
-      handleInput (evt) {
-        const value = evt.target.value
-        this.$emit('input', value)
-        this.$emit('change', value)
-      },
-      setCurrentValue (val) {
-        if (val === this.currentValue) return
-        this.currentValue = val
-        this.dispatch('AtFormItem', 'on-form-item-change', this.currentValue)
-      }
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    },
+    maxlength: Number,
+    minlength: Number,
+    max: Number,
+    min: Number
+  },
+  computed: {
+    iconClass () {
+      const name = this.icon || this.status
+      return name ? `icon-${name}` : ''
+    }
+  },
+  data () {
+    return {
+      currentValue: this.value
+    }
+  },
+  watch: {
+    value (val) {
+      this.setCurrentValue(val)
+    }
+  },
+  methods: {
+    handleFocus (evt) {
+      this.$emit('focus', evt)
+    },
+    handleBlur (evt) {
+      this.$emit('blur', evt)
+      this.dispatch('AtFormItem', 'on-form-item-blur', this.currentValue)
+    },
+    handleInput (evt) {
+      const value = evt.target.value
+      this.$emit('input', value)
+      this.$emit('change', value)
+    },
+    setCurrentValue (val) {
+      if (val === this.currentValue) return
+      this.currentValue = val
+      this.dispatch('AtFormItem', 'on-form-item-change', this.currentValue)
     }
   }
 }
