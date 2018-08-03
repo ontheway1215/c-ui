446d83f54fd6bc7e6e489e50441150b044b01e3a
ontheway1215 <179023084@qq.com>
2018-08-03 15:19:48 +0800

    change checkbox bug

--- src/components/checkbox/src/checkbox.vue
+++ src/components/checkbox/src/checkbox.vue
@@ -37,8 +37,8 @@
 </template>
 
 <script>
-import { findComponentUpward } from 'at-ui/src/utils/util'
-import Emitter from 'at-ui/src/mixins/emitter'
+import { findComponentUpward } from 'c-ui/src/utils/util'
+import Emitter from 'c-ui/src/mixins/emitter'
 export default {
   name: 'AtCheckbox',
   mixins: [Emitter],
